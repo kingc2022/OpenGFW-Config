d1366f6c1581907d68e5b1938d222a66fd344e89
kingc <99593855+kingc2022@users.noreply.github.com>
2024-08-27 01:34:27 +0800

    Update install.sh

--- shell/install.sh
+++ shell/install.sh
@@ -5,19 +5,18 @@ apt update
 apt install wget
 
 echo "Make directory"
-mkdir -p /opt
-mkdir -p /opt/opengfw
+mkdir -p /root/opengfw
 
 echo "Download OpenGFW v0.4.0 amd64"
-wget https://proxy-gh.1l1.icu/github.com/apernet/OpenGFW/releases/download/v0.4.0/OpenGFW-linux-amd64 -O /opt/opengfw/OpenGFW
-chmod +x /opt/opengfw/OpenGFW
+wget https://mirror.ghproxy.com/github.com/apernet/OpenGFW/releases/download/v0.4.0/OpenGFW-linux-amd64 -O /root/opengfw/OpenGFW
+chmod +x /root/opengfw/OpenGFW
 
 echo "Download config.yml and rules.yml"
-wget https://proxy-gh.1l1.icu/github.com/MuskaNet/OpenGFW-configuration/raw/main/config.yml -O /opt/opengfw/config.yml
-wget https://proxy-gh.1l1.icu/github.com/MuskaNet/OpenGFW-configuration/raw/main/rules.yml -O /opt/opengfw/rules.yml
+wget https://mirror.ghproxy.com/github.com/kingc2022/OpenGFW-configuration/raw/main/config.yml -O /root/opengfw/config.yml
+wget https://mirror.ghproxy.com/github.com/kingc2022/OpenGFW-configuration/raw/main/rules-china.yml -O /root/opengfw/rules.yml
 
 echo "Download opengfw.service"
-wget https://proxy-gh.1l1.icu/github.com/MuskaNet/OpenGFW-configuration/raw/main/daemon/opengfw.service -O /usr/lib/systemd/system/opengfw.service
+wget https://mirror.ghproxy.com/github.com/kingc2022/OpenGFW-configuration/raw/main/daemon/opengfw.service -O /usr/lib/systemd/system/opengfw.service
 
 echo "Enable and start OpenGFW"
 systemctl enable --now opengfw.service
